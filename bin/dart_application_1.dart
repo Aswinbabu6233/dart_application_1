6869532b921ee4c0c3c313f373bcfb7d792e6dcd
Aswinbabu6233 <aswinbabu6933@gmail.com>
2024-02-26 10:57:38 +0530

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main() {
+  String name = "Aswin";
+  String email = "Aswinbabu6933@gmail.com";
+  int age = 21;
+  double cgpa = 7.53;
+
+  print("NAME: $name");
+  print("Email: $email");
+  print("age: $age");
+  print("Cgpa: $cgpa");
+}
